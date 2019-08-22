1c04538581848868b4132f4e715498c640ea7e52
donghao.break <donghao.break@bytedance.com>
2019-08-22 14:00:48 +0800

    refactor with functional

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
-    let query = args[1].clone();
-    let filename = args[2].clone();
+  pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+    args.next();
+
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a query string"),
+    };
+
+    let filename = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a filename string"),
+    };
     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     Ok(Config {
       query,
@@ -74,23 +80,15 @@ Trust me.";
 
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
   let query = query.to_lowercase();
-  let mut results = Vec::new();
-
-  for line in content.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-  results
+  content
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&query))
+    .collect()
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in content.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-  results
+  content
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
